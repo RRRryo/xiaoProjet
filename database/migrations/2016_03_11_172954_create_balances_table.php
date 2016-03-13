f0529f03a00e540f83c8466b6557baf1749febae
Liang <liang6872@gmail.com>
2016-03-14 00:55:16 +0800

    lei

---
+++ database/migrations/2016_03_11_172954_create_balances_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBalancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('balances', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->decimal('balance', 10, 2);
+            $table->string('description');
+            $table->integer('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('balances');
+    }
+}
